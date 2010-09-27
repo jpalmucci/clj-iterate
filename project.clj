3f56b1b3f70cb704696391d8a4ad23ac8c4439b7
Jeff Palmucci <jpalmucci@csail.mit.edu>
2010-09-27 08:24:52 -0400

    .92 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-iterate/clj-iterate "0.92-SNAPSHOT"
+(defproject clj-iterate/clj-iterate "0.92"
   :description "An iteration macro along the lines of Common Lisp Iterate."
   :dependencies [[org.clojure/clojure "1.2.0"] 
                  [org.clojure/clojure-contrib "1.2.0"]] )
